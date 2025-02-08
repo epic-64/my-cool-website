c318f2ba7bb5f7043d0065c15c73a7be94d64d84
William Raendchen <william.raendchen@easybell.de>
2025-02-08 15:33:52 +0100

    init

--- build.sbt
+++ build.sbt
@@ -7,11 +7,12 @@ lazy val root = (project in file("."))
   )
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka"             %% "akka-http"    % "10.5.3",
-  "com.typesafe.akka"             %% "akka-stream"  % "2.8.8",
-  "com.lihaoyi"                   %% "upickle"      % "4.1.0",
-  "com.softwaremill.sttp.client4" %% "core"         % "4.0.0-RC1",
-  "org.scalatest"                 %% "scalatest"    % "3.2.19"   % Test,
-  "org.scalatestplus"             %% "mockito-5-12" % "3.2.19.0" % Test,
-  "org.scalamock"                 %% "scalamock"    % "7.2.0"    % Test,
+  "com.typesafe.akka"             %% "akka-http"            % "10.5.3",
+  "com.typesafe.akka"             %% "akka-stream"          % "2.8.8",
+  "com.typesafe.akka"             %% "akka-http-spray-json" % "10.5.3",
+  "com.lihaoyi"                   %% "upickle"              % "4.1.0",
+  "com.softwaremill.sttp.client4" %% "core"                 % "4.0.0-RC1",
+  "org.scalatest"                 %% "scalatest"            % "3.2.19"   % Test,
+  "org.scalatestplus"             %% "mockito-5-12"         % "3.2.19.0" % Test,
+  "org.scalamock"                 %% "scalamock"            % "7.2.0"    % Test,
 )
--- src/main/scala/HelloWorldServer.scala
+++ src/main/scala/HelloWorldServer.scala
@@ -1,11 +1,20 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
+import akka.http.scaladsl.server.Directives.*
+import spray.json.DefaultJsonProtocol.* // Provides JSON formatting
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
+// ✅ Define a case class for the JSON response
+case class SumResult(x: Int, y: Int, sum: Int)
+
+// ✅ Create a JSON formatter (needed for automatic conversion)
+given spray.json.RootJsonFormat[SumResult] = jsonFormat3(SumResult.apply)
+
 object HelloWorldServer {
+
   def main(args: Array[String]): Unit = {
     implicit val system: ActorSystem                        = ActorSystem("my-actor-system")
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
@@ -15,7 +24,11 @@ object HelloWorldServer {
       path("goodbye")(complete("Goodbye, world!")),
       path("ping")(complete("pong")),
       path("hello" / Segment)(name => complete(s"Hello, $name!")),
-      path("add" / IntNumber / IntNumber)((x, y) => complete(s"The sum is: ${x + y}"))
+
+      // ✅ JSON response for /add/{x}/{y}
+      path("add" / IntNumber / IntNumber) { (x, y) =>
+        complete(SumResult(x, y, x + y)) // Automatically converts to JSON
+      }
     )
 
     val bindingFuture = Http().newServerAt("localhost", 8080).bind(routes)
