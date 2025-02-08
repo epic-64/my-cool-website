fb65d563b9bd0763490be4454b926ed1a8f57446
William Raendchen <william.raendchen@easybell.de>
2025-02-08 15:17:12 +0100

    init

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / version      := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "3.6.3"
 
 lazy val root = (project in file("."))
@@ -7,6 +7,11 @@ lazy val root = (project in file("."))
   )
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-http"  % "10.5.3",
-  "com.typesafe.akka" %% "akka-stream" % "2.8.8"
+  "com.typesafe.akka"             %% "akka-http"    % "10.5.3",
+  "com.typesafe.akka"             %% "akka-stream"  % "2.8.8",
+  "com.lihaoyi"                   %% "upickle"      % "4.1.0",
+  "com.softwaremill.sttp.client4" %% "core"         % "4.0.0-RC1",
+  "org.scalatest"                 %% "scalatest"    % "3.2.19"   % Test,
+  "org.scalatestplus"             %% "mockito-5-12" % "3.2.19.0" % Test,
+  "org.scalamock"                 %% "scalamock"    % "7.2.0"    % Test,
 )
--- src/main/scala/HelloWorldServer.scala
+++ src/main/scala/HelloWorldServer.scala
@@ -8,25 +8,23 @@ import scala.io.StdIn
 object HelloWorldServer {
 
   def main(args: Array[String]): Unit = {
-    // Create an Actor System
-    implicit val system: ActorSystem = ActorSystem("my-actor-system")
+    implicit val system: ActorSystem                        = ActorSystem("my-actor-system")
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    // Define route
-    val route =
-      path("hello") {
-        get(complete("Hello, world!"))
-      }
+    // Define multiple routes
+    val routes = concat(
+      path("hello")    { complete("Hello, world!") },
+      path("goodbye")  { complete("Goodbye, world!") },
+      path("ping")     { complete("pong") }
+    )
 
-    // Start HTTP server on port 8080
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(routes)
 
     println("Server running at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // Wait for user to press Enter
 
-    // Shutdown
     bindingFuture
-      .flatMap(_.unbind()) // Unbind from the port
+      .flatMap(_.unbind())                 // Unbind the port
       .onComplete(_ => system.terminate()) // Shut down actor system
   }
 }
