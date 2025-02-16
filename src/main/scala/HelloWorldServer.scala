6e10e5d675bfbc0ef28cbaaf41aecac98fe13002
epic-64 <william.raendchen@gmail.com>
2025-02-17 00:00:00 +0100

    add some random bullshit for railway

--- src/main/scala/HelloWorldServer.scala
+++ src/main/scala/HelloWorldServer.scala
@@ -9,7 +9,9 @@ object HelloWorldServer:
     implicit val system: ActorSystem                        = ActorSystem("my-actor-system")
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(Routes.allRoutes)
+    val port          = sys.env.getOrElse("PORT", "8080").toInt
+    val bindingFuture = Http().newServerAt("0.0.0.0", port).bind(Routes.allRoutes)
+
     println("Server running at http://localhost:8080/")
 
     Await.result(bindingFuture.flatMap(_.whenTerminated), Duration.Inf)
