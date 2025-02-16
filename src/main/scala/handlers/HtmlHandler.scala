82e5e5ee71d03155ef1bb3b57459b9385681a162
epic-64 <william.raendchen@gmail.com>
2025-02-17 00:00:00 +0100

    add file based counter

--- src/main/scala/handlers/HtmlHandler.scala
+++ src/main/scala/handlers/HtmlHandler.scala
@@ -4,10 +4,37 @@ import org.apache.pekko.http.scaladsl.model.headers.HttpCookie
 import org.apache.pekko.http.scaladsl.model.{ContentTypes, HttpEntity}
 import org.apache.pekko.http.scaladsl.server.Directives.*
 import org.apache.pekko.http.scaladsl.server.Route
+import upickle.default.*
 
-import java.nio.file.Paths
+import java.nio.file.{Files, Paths, StandardOpenOption}
 import scala.collection.concurrent.TrieMap
-import scala.util.Random
+import scala.util.{Random, Try}
+
+case class UserCounter(counter: Int)
+
+object UserCounter:
+  given ReadWriter[UserCounter] = macroRW
+
+object UserCounterStorage {
+  private def userFilePath(userId: String) = Paths.get(s"data/user_$userId.json")
+
+  def readCounter(userId: String): UserCounter =
+    val path = userFilePath(userId)
+    if Files.exists(path) then
+      val content = Files.readString(path)
+      Try(read[UserCounter](content)).getOrElse(UserCounter(0))
+    else UserCounter(0)
+
+  def writeCounter(userId: String, counter: UserCounter): Unit =
+    val path = userFilePath(userId)
+    Files.createDirectories(path.getParent)
+    Files.writeString(
+      path,
+      write(counter, indent = 2), // Pretty print for easy reading
+      StandardOpenOption.CREATE,
+      StandardOpenOption.TRUNCATE_EXISTING
+    )
+}
 
 def withUserSession(innerRoute: String => Route): Route =
   optionalCookie("userId") {
@@ -39,21 +66,26 @@ def renderHelloTwirl(name: String): String =
 
 val userCounters = TrieMap[String, Int]()
 
-def renderPong(userId: String): String =
+def renderPong(userId: String): String = {
   val emojis = List(
-    ("🏓", "red"),
-    ("🏸", "blue"),
-    ("🏒", "green"),
-    ("🏏", "yellow"),
-    ("🏑", "purple")
+    ("🏓", "#FFAADD"),
+    ("🏓", "#FFBBDD"),
+    ("🏓", "#FFCCDD"),
+    ("🏓", "#FFDDDD"),
+    ("🏓", "#FFEEDD"),
+    ("🏓", "#FFDDDD"),
+    ("🏓", "#FFCCDD"),
+    ("🏓", "#FFBBDD"),
   )
 
-  val currentIndex = userCounters.getOrElseUpdate(userId, 0)
-  val nextIndex    = (currentIndex + 1) % emojis.size
-  userCounters.update(userId, nextIndex)
+  val current   = UserCounterStorage.readCounter(userId)
+  val nextIndex = (current.counter + 1) % emojis.size
 
-  val (emoji, color) = emojis(currentIndex)
+  UserCounterStorage.writeCounter(userId, UserCounter(nextIndex))
+
+  val (emoji, color) = emojis(current.counter)
   views.html.pong(emoji, color).toString
+}
 
 extension (content: String) def toUtf8Http: Route = complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, content))
 
