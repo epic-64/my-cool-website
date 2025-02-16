747399b0697dccf14d9ac955e3c7b9cd0670ed88
epic-64 <william.raendchen@gmail.com>
2025-02-17 00:00:00 +0100

    cleanup

--- src/main/scala/handlers/HtmlHandler.scala
+++ src/main/scala/handlers/HtmlHandler.scala
@@ -22,15 +22,14 @@ def renderHelloTwirl(name: String): String =
   views.html.hello(name, getFrontendVersion).toString
 
 def renderPong: String =
-  val emojis = List("🏓", "🏸", "🏒", "🏏", "🏑", "🏹", "🎣", "🥊", "🥋", "🥅", "🎯")
+  val emojis      = List("🏓", "🏸", "🏒", "🏏", "🏑", "🏹", "🎣", "🥊", "🥋", "🥅", "🎯")
   val randomEmoji = emojis(scala.util.Random.nextInt(emojis.length))
   views.html.pong(randomEmoji).toString
 
-extension (content: String)
-  def toUtf8Http: Route = complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, content))
+extension (content: String) def toUtf8Http: Route = complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, content))
 
 object HtmlHandler:
   def routes: Route = concat(
-    path("hello" / Segment) { name => renderHelloTwirl(name).toUtf8Http },
-    path("ping") { renderPong.toUtf8Http }
+    path("hello" / Segment)(name => renderHelloTwirl(name).toUtf8Http),
+    path("ping")(renderPong.toUtf8Http)
   )
